020fd0257b28b3a8efe07149f1825c4582066fc3
KevAndrews <nivek87@gmail.com>
2021-06-17 14:53:10 +0100

    Add base tile check in maingame JS

--- assets/js/maingame.js
+++ assets/js/maingame.js
@@ -3,16 +3,16 @@ $(document).ready(function() {
     loadMap();
 
     $(".green-div").click(function() {
-        alert("Div");
+        checkTile(this);
     });
 });
 
 function loadMap(){
     let map = [
         [1,1,1,1,1,1,1],
-        [1,0,0,0,0,0,1],
-        [1,0,1,1,1,0,1],
-        [1,0,0,0,0,0,1],
+        [1,0,0,1,0,0,1],
+        [1,0,1,2,1,0,1],
+        [1,0,0,1,0,0,1],
         [1,1,1,1,1,1,1]
     ];
 
@@ -21,14 +21,33 @@ function loadMap(){
     for (let i = 0; i < map.length; i++) {
       for(let l = 0; l < map[i].length; l++){
         if(map[i][l] == 1){
-            document.getElementById('game-world').innerHTML += `<div class="game-tile green-div"></div>`;
+            $('#game-world').append('<div class="game-tile green-div"></div>');
+        } else if((map[i][l] == 2))
+        {
+            $('#game-world').append('<div class="game-tile green-div"><div class="player-div"></div></div>');
         } else{
-            document.getElementById('game-world').innerHTML += `<div class="game-tile red-div"></div>`;
+            $('#game-world').append('<div class="game-tile red-div"></div>');
         } 
       }
     }
 }
 
-function checkTile(){
+function checkTile(selectedDiv){
+    
+    if($(selectedDiv).children().hasClass("player-div")){
+        alert("Already Here");
+    } 
+        
+    //Check 4 dircetions
+    //Up
+    alert($(selectedDiv).prevAll().eq(6).children().hasClass("player-div") + " Up");
+    //Down
+    alert($(selectedDiv).nextAll().eq(6).children().hasClass("player-div") + " Down");
+    //left
+    alert($(selectedDiv).prev().children().hasClass("player-div") + " left");
+    //right
+    alert($(selectedDiv).next().children().hasClass("player-div") + " right");
+
+    //alert($(selectedDiv).prev().children().hasClass("player-div"));
 
 }
