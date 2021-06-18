daa5082c4959fc3a26feead75f4cc995b510e944
KevAndrews <nivek87@gmail.com>
2021-06-18 12:57:11 +0100

    Add Movement to maingame JS

--- assets/js/maingame.js
+++ assets/js/maingame.js
@@ -5,15 +5,20 @@ $(document).ready(function() {
     $(".green-div").click(function() {
         checkTile(this);
     });
+
+    $(".blue-div").click(function() {
+        checkTile(this);
+    });
+
 });
 
 function loadMap(){
     let map = [
-        [1,1,1,1,1,1,1],
-        [1,0,0,1,0,0,1],
-        [1,0,1,2,1,0,1],
-        [1,0,0,1,0,0,1],
-        [1,1,1,1,1,1,1]
+        [0,0,0,0,0,0,0],
+        [0,0,0,1,1,0,0],
+        [2,1,1,1,1,1,3],
+        [0,0,1,1,1,1,0],
+        [0,0,0,0,0,0,0]
     ];
 
     document.getElementById('game-world').innerHTML = "";
@@ -22,9 +27,10 @@ function loadMap(){
       for(let l = 0; l < map[i].length; l++){
         if(map[i][l] == 1){
             $('#game-world').append('<div class="game-tile green-div"></div>');
-        } else if((map[i][l] == 2))
-        {
+        } else if((map[i][l] == 2)){
             $('#game-world').append('<div class="game-tile green-div"><div class="player-div"></div></div>');
+        } else if((map[i][l] == 3)){
+            $('#game-world').append('<div class="game-tile blue-div"></div>');
         } else{
             $('#game-world').append('<div class="game-tile red-div"></div>');
         } 
@@ -37,17 +43,59 @@ function checkTile(selectedDiv){
     if($(selectedDiv).children().hasClass("player-div")){
         alert("Already Here");
     } 
-        
-    //Check 4 dircetions
-    //Up
-    alert($(selectedDiv).prevAll().eq(6).children().hasClass("player-div") + " Up");
-    //Down
-    alert($(selectedDiv).nextAll().eq(6).children().hasClass("player-div") + " Down");
-    //left
-    alert($(selectedDiv).prev().children().hasClass("player-div") + " left");
-    //right
-    alert($(selectedDiv).next().children().hasClass("player-div") + " right");
-
-    //alert($(selectedDiv).prev().children().hasClass("player-div"));
+    
+    if($(selectedDiv).hasClass("green-div")){
+        //Check 4 dircetions
+        //Up
+        if($(selectedDiv).prevAll().eq(6).children().hasClass("player-div")){
+            $(selectedDiv).prevAll().eq(6).children().remove();
+            $(selectedDiv).prevAll().eq(6).removeClass("green-div");
+            $(selectedDiv).html('<div class="player-div"></div>');
+        }
+        //Down
+        if($(selectedDiv).nextAll().eq(6).children().hasClass("player-div")){
+            $(selectedDiv).nextAll().eq(6).children().remove();
+            $(selectedDiv).nextAll().eq(6).removeClass("green-div");
+            $(selectedDiv).html('<div class="player-div"></div>');
+        }
+        //left
+        if($(selectedDiv).prev().children().hasClass("player-div")){
+            $(selectedDiv).prev().children().remove();
+            $(selectedDiv).prev().removeClass("green-div");
+            $(selectedDiv).html('<div class="player-div"></div>');
+        }
+        //right
+        if($(selectedDiv).next().children().hasClass("player-div")){
+            $(selectedDiv).next().children().remove();
+            $(selectedDiv).next().removeClass("green-div");
+            $(selectedDiv).html('<div class="player-div"></div>');
+        }
+    } else if($(selectedDiv).hasClass("blue-div")){
+        //Check 4 dircetions
+        //Up
+        if($(selectedDiv).prevAll().eq(6).children().hasClass("player-div")){
+            $(selectedDiv).prevAll().eq(6).children().remove();
+            $(selectedDiv).prevAll().eq(6).removeClass("green-div");
+            $(selectedDiv).html('<div class="player-div"></div>');
+        }
+        //Down
+        if($(selectedDiv).nextAll().eq(6).children().hasClass("player-div")){
+            $(selectedDiv).nextAll().eq(6).children().remove();
+            $(selectedDiv).nextAll().eq(6).removeClass("green-div");
+            $(selectedDiv).html('<div class="player-div"></div>');
+        }
+        //left
+        if($(selectedDiv).prev().children().hasClass("player-div")){
+            $(selectedDiv).prev().children().remove();
+            $(selectedDiv).prev().removeClass("green-div");
+            $(selectedDiv).html('<div class="player-div"></div>');
+        }
+        //right
+        if($(selectedDiv).next().children().hasClass("player-div")){
+            $(selectedDiv).next().children().remove();
+            $(selectedDiv).next().removeClass("green-div");
+            $(selectedDiv).html('<div class="player-div"></div>');
+        }
+    }
 
 }
