0a7dd0cc1aeb6c8ff4990181b3323c6204145523
KevAndrews <nivek87@gmail.com>
2021-06-18 13:59:03 +0100

    Add game functions Add Timer Add Score Add end of level

--- assets/js/maingame.js
+++ assets/js/maingame.js
@@ -1,18 +1,74 @@
 /* Custom Code */
 $(document).ready(function() {
-    loadMap();
-
-    $(".green-div").click(function() {
-        checkTile(this);
-    });
-
-    $(".blue-div").click(function() {
-        checkTile(this);
-    });
+    
+    runGame();
 
 });
 
-function loadMap(){
+function runGame(){
+    //set game world parameters
+    let level = 1;
+    let score = 0;
+    //Load Map/level
+    loadLevel(level);
+    
+    $(".green-div").click(function() {
+        canMove(this);
+    });
+
+    $(".blue-div").click(function() {
+        if(canMove(this)){
+            endLevel(level);
+            alert(endLevelScore());
+        }
+    });
+
+}
+
+function endLevel(level){
+	$("#level").html(++level);
+    return ++level;
+}
+
+function endLevelScore(){
+    let oldScore = parseInt($("#score").text());
+	let finishTime = parseInt($("#time").text());
+
+    let levelScore = (oldScore * 10) + (finishTime * 10);
+
+    return levelScore;
+}
+
+function restartLevel(){
+
+}
+
+function returnToMenu(){
+
+}
+
+/*
+ Code used from:
+ https://stackoverflow.com/questions/2604450/how-to-create-a-jquery-clock-timer
+*/
+let elapsed_seconds = 60;
+setInterval(function() {
+  elapsed_seconds = --elapsed_seconds;
+  $('#time').text(elapsed_seconds);
+  if(elapsed_seconds <= 0){
+      alert("Game Over");
+      elapsed_seconds = 60;
+  }
+}, 1000);
+
+// Used the JavaScript_walk_through_challenge as a template
+function incrementScore() {
+	// Gets the current score from the DOM and increments it
+	let oldScore = parseInt($("#score").text());
+	$("#score").html(++oldScore);
+}
+
+function loadLevel(level){
     let map = [
         [0,0,0,0,0,0,0],
         [0,0,0,1,1,0,0],
@@ -21,7 +77,7 @@ function loadMap(){
         [0,0,0,0,0,0,0]
     ];
 
-    document.getElementById('game-world').innerHTML = "";
+    $('#game-world').empty();
 
     for (let i = 0; i < map.length; i++) {
       for(let l = 0; l < map[i].length; l++){
@@ -38,14 +94,16 @@ function loadMap(){
     }
 }
 
-function checkTile(selectedDiv){
+function canMove(selectedDiv){
     
+    //Check if the player is on the current Tile
     if($(selectedDiv).children().hasClass("player-div")){
-        alert("Already Here");
+        alert("You are on the current tile");
     } 
     
+    //Check if the selected Tile can be moved to, this is horrible and needs to be corrected 
     if($(selectedDiv).hasClass("green-div")){
-        //Check 4 dircetions
+        //Check 4 directions around the selected Tile for the player and if so move
         //Up
         if($(selectedDiv).prevAll().eq(6).children().hasClass("player-div")){
             $(selectedDiv).prevAll().eq(6).children().remove();
@@ -77,25 +135,30 @@ function checkTile(selectedDiv){
             $(selectedDiv).prevAll().eq(6).children().remove();
             $(selectedDiv).prevAll().eq(6).removeClass("green-div");
             $(selectedDiv).html('<div class="player-div"></div>');
+            return true;
         }
         //Down
         if($(selectedDiv).nextAll().eq(6).children().hasClass("player-div")){
             $(selectedDiv).nextAll().eq(6).children().remove();
             $(selectedDiv).nextAll().eq(6).removeClass("green-div");
             $(selectedDiv).html('<div class="player-div"></div>');
+            return true;
         }
         //left
         if($(selectedDiv).prev().children().hasClass("player-div")){
             $(selectedDiv).prev().children().remove();
             $(selectedDiv).prev().removeClass("green-div");
             $(selectedDiv).html('<div class="player-div"></div>');
+            return true;
         }
         //right
         if($(selectedDiv).next().children().hasClass("player-div")){
             $(selectedDiv).next().children().remove();
             $(selectedDiv).next().removeClass("green-div");
             $(selectedDiv).html('<div class="player-div"></div>');
+            return true;
         }
     }
 
 }
+
