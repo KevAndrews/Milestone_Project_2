240e07cfec622512cfbcd4b32f7a4e8ba6a87c4f
KevAndrews <nivek87@gmail.com>
2021-06-20 13:27:55 +0100

    Add audio controls to the main menu

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -1,8 +1,22 @@
+/* Custom Code */
 document.addEventListener('DOMContentLoaded', (event) => {
     let bgMusic = new sound("http://127.0.0.1:5500/assets/audio/bg_music.mp3");
-    bgMusic.play();
+    document.getElementById("audio-Controller").addEventListener("click", function(){ soundController(bgMusic, this); });
 });
 
+// Toggle Music on / off
+function soundController(bgMusic, element){
+
+    if(bgMusic.paused){
+        element.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i> Pause Music';
+        bgMusic.play();
+        bgMusic.paused = false;
+    } else{
+        element.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i> Play Music';
+        bgMusic.stop();
+        bgMusic.paused = true;
+    }
+}
 
 /* Code from https://www.w3schools.com/graphics/game_sound.asp */ 
 function sound(src) {
@@ -13,6 +27,7 @@ function sound(src) {
     this.sound.style.display = "none";
     this.sound.volume = 0.2;
     this.sound.loop = true;
+    this.paused = true;
     document.body.appendChild(this.sound);
     this.play = function(){
         this.sound.play();
